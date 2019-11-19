558cffbc1a635537e82c49d57477a301ad73d464
Naman Arora <xavier.arora@gmail.com>
2019-11-19 19:06:32 +0530

    add stock messages

---
+++ messages.py
@@ -0,0 +1,39 @@
+Messsages = {
+    "informative": [
+        "Tell me more about you.",
+        "What are your hobbies?",
+        "How was your day?",
+        "What kinda music do you enjoy listening to?",
+        "Do you have a pet?",
+        "Who is in your family",
+        "Tell me whether you like sports, when you last went out to play and which sport you like.",
+    ],
+    "greeting": [
+        "Hey there, How's it going?",
+        "Hello!",
+        "Hi! I'm Happy, the cheerbot",
+        "Hey cutie"
+    ],
+    "positive": [
+        "Wonderful!",
+        "Yeah!",
+        "That's great! Tell me more!",
+        "I'm glad! :D\n",
+        "Cool :) \n",
+    ],
+    "subject_responses": [
+        "Tell me more about it.",
+        "Oh! what else..",
+        "That's nice.",
+        "Uh-huh",
+        "Seems interesting, tell me more."
+    ],
+    "negative": [
+        "I see...",
+        "Hard luck...",
+        "Cheer up. It's gonna be fine."
+    ],
+    "911": [
+        "I'm sorry to hear this about it.",
+    ]
+}
